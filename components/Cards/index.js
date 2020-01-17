46245471e8441d8971d88ebaeefaca8e6245cee4
lgv-0 <55011072+lgv-0@users.noreply.github.com>
2020-01-17 11:48:01 -0500

    CardCreate

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,38 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function createCard(o_Data)
+{
+    let Div = document.createElement("div");
+    Div.setAttribute("class", "card");
+
+    Div.appendChild(Object.assign(document.createElement("div"), {"innerText":o_Data["headline"]}));
+    Div.setLastAttr("class", "headline");
+
+    //////////////////////////////////////////
+    let AuthorDiv = document.createElement("div");
+    AuthorDiv.setAttribute("class", "author");
+
+    AuthorDiv.appendChild(document.createElement("div"));
+    AuthorDiv.setLastAttr("class", "img-container");
+    
+    AuthorDiv.children[AuthorDiv.children.length - 1].appendChild(document.createElement("img"));
+    AuthorDiv.children[AuthorDiv.children.length - 1].setLastAttr("src", o_Data["authorPhoto"]);
+
+    AuthorDiv.appendChild(Object.assign(document.createElement("span"), {"innerText":`By ${o_Data["authorName"]}`}));
+
+    Div.appendChild(AuthorDiv);
+    //////////////////////////////////////////
+
+    return Div;
+}
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles").then((response)=>
+    {
+        let CardContainer = document.querySelector(".cards-container");
+        let a_S1 = Object.keys(response.data.articles);
+        for (let i = 0; i < a_S1.length; i++)
+            for (let z = 0; z < response.data.articles[a_S1[i]].length; z++)
+                CardContainer.appendChild(createCard(response.data.articles[a_S1[i]][z]));
+    }).catch(error => {console.log(error.response)});
